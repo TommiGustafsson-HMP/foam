4a87a93fb0f49e27377ed0f4e9b1651390d4f85b
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2025-07-28 15:13:18 +0300

    Add support for img tag rendering with root relative src.

--- packages/foam-vscode/src/features/preview/index.ts
+++ packages/foam-vscode/src/features/preview/index.ts
@@ -6,6 +6,25 @@ import { default as markdownItFoamTags } from './tag-highlight';
 import { default as markdownItWikilinkNavigation } from './wikilink-navigation';
 import { default as markdownItRemoveLinkReferences } from './remove-wikilink-references';
 import { default as markdownItWikilinkEmbed } from './wikilink-embed';
+import * as path from 'path';
+
+export function htmlRule (tokens, idx, options, env, self) {
+  const token = tokens[idx];
+  
+  if (token.content.startsWith('<img ')) {
+    const hasRootSrc = token.content.indexOf('src="/') >= 0;
+    if(hasRootSrc) {
+      const newContent = token.content.replace(/src="(\/[^"]+)"/, function (match, p1) {
+        const workspaceFolder = vscode.workspace.workspaceFolders[0];
+        const srcPath = path.join(workspaceFolder.uri.fsPath, p1).replace(/\\/g, "/");
+        const vsCodePath = "https://file+.vscode-resource.vscode-cdn.net/" + srcPath;
+        return 'src="' + vsCodePath + '"';
+      });
+      return newContent
+    }
+  }
+  return token.content;
+}
 
 export default async function activate(
   context: vscode.ExtensionContext,
@@ -15,6 +34,8 @@ export default async function activate(
 
   return {
     extendMarkdownIt: (md: markdownit) => {
+      md.renderer.rules.html_inline = htmlRule;
+
       return [
         markdownItWikilinkEmbed,
         markdownItFoamTags,
